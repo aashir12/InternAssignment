b9457db3ba7d41acb227d07f18698bb156b393f8
Aashir <aashirnawaz5@gmail.com>
2024-03-23 23:09:33 +0500

    uploading

--- client/src/Components/cardCreate.jsx
+++ client/src/Components/cardCreate.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const CardCreate = () => {
+const CardCreate = ({ id, url, Arther,click,styler }) => {
   return (
-<>
-  <div className='cardCover'>
-    <div className='image'></div>
-    <h4>Create a new project</h4>
-    <h5>or try a <span style={{color:'rgb(250, 120, 47)'}}>sample project</span></h5>
-  </div>
-</>
-  )
-}
+    <>
+   <div className='cardCover' onClick={click} style={styler}>
 
-export default CardCreate
+        <div className='image'><img src="/images/new.PNG"/></div>
+        <h4>{Arther}</h4>
+        <h5>or try a <span style={{ color: 'rgb(250, 120, 47)' }}>sample project</span></h5>
+        <div className='readDel'>
+        <h5 style={{cursor:'pointer'}}>Read More-></h5>
+        <h5 style={{color:'red',cursor:'pointer'}}>Delete</h5>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardCreate;
--- client/src/Components/cover.jsx
+++ client/src/Components/cover.jsx
@@ -1,40 +1,96 @@
-import React from 'react'
-import Header from './header'
+import React, { useEffect, useState } from 'react';
+import Header from './header';
+import CardCreate from './cardCreate';
+import axios from 'axios';
 
 const Cover = () => {
-  return (
-    <div className='cover'>
-        <div className='coverlft'>
-            <div className='logo'>Necleo</div>
-            <hr/>
-            <div className='lftCol'>
-            <div className='colTop'>
-            <h3>My Projects</h3>
-                <h3 style={{color:"rgb(250, 120, 47)"}}>Sample Projects</h3>
-                <hr/>
-                <h3>Apps</h3>
-                <h3>Intro to Necleo</h3>
+    const [data, setData] = useState([]);
+    const [checker, setChecker] = useState(0);
+    const [col, setCol] = useState(true);
+    const [item, setItem] = useState({
+        arther: '',
+        link: ''
+    });
+
+    useEffect(() => {
+        axios.get('https://picsum.photos/v2/list?page=1&limit=6')
+            .then((res) => {
+                setData(res.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    const handleCardClick = () => (
+        setChecker(1)
+    );
+
+    const Remove = () => {
+        setChecker(0);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setData([...data, item]);
+        setChecker(0); 
+        setItem({ arther: '', link: '' }); 
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setItem(prev => ({ ...prev, [name]: value }));
+    };
+
+    return (
+        <div className='cover'>
+            <div className='coverlft'>
+                <div className='logo'>
+                    <img src='/images/logo.PNG' style={{ width: '100%', borderBottom: "0.3px solid grey", paddingBottom: "10%",cursor:'pointer' }} onClick={()=>{setCol(!col)}}/>
+                </div>
+                <div className='lftCol' style={col===true?{marginLeft:'0',transition:'0.5s'}:{marginLeft:'-250px',transition:'0.5s'}}>
+                    <div className='colTop'>
+                        <h3 style={{color: "rgb(250, 120, 47)"}}><i class="fa-solid fa-database" style={{paddingRight:'10%',paddingTop:'10%'}}></i>My Projects</h3>
+                        <h3 style={{  borderBottom: "0.3px solid grey", paddingBottom: "10%" }}><i class="fa-solid fa-table-cells-large" style={{paddingRight:'10%',paddingTop:'10%'}}></i>Sample Projects</h3>
+                        <h3><i class="fa-brands fa-adn" style={{paddingRight:'10%',paddingTop:'10%'}}></i>Apps</h3>
+                        <h3><i class="fa-solid fa-circle-play" style={{paddingRight:'10%',paddingTop:'10%'}}></i>Intro to Necleo</h3>
+                    </div>
+                    <div className='colBottom'>
+                        <h3><i class="fa-solid fa-circle-question" style={{paddingRight:'10%',paddingTop:'10%'}}></i>Help and Support</h3>
+                        <h3><i class="fa-solid fa-comment-dots" style={{paddingRight:'10%',paddingTop:'10%'}}></i>Feedback</h3>
+                        <h3><i class="fa-solid fa-circle-arrow-left" style={{paddingRight:'10%',paddingTop:'10%'}} onClick={()=>setCol(!col)}></i>Collapse</h3>
+                    </div>
+                </div>
             </div>
-               <div className='colBottom'>
-               <h3>Help and Support</h3>
-                <h3>FeedBacks</h3>
-                <h3>Collapse</h3>
-               </div>
-               <div>
-
-               </div>
-                
-
+            <div className='coverrft'>
+                <div>
+                    <Header />
+                </div>
+                <div className='content'>
+                    {checker === 1 &&
+                        <div className="form-popup" id="myForm">
+                            <form onSubmit={handleSubmit} className="form-container">
+                                <h1>Add a Post</h1>
+                                <label htmlFor="arther"><b>Arther Name</b></label>
+                                <input type="text" placeholder="John Allen" name="arther" value={item.arther} onChange={handleChange} required />
+                                <label htmlFor="link"><b>Image Link</b></label>
+                                <input type="text" placeholder="www.link.com" name="link" value={item.link} onChange={handleChange} required />
+                                <button type="submit" className="btn">Submit</button>
+                                <button type="button" className="btn cancel" onClick={Remove}>Close</button>
+                            </form>
+                        </div>
+                    }
+                    <h1>My Projects</h1>
+                    <div className='cards'>
+                        <CardCreate url="/images/new.PNG" Arther="Add New" styler={{ cursor: "pointer" }} click={handleCardClick} />
+                        {data.map((item) => (
+                            <CardCreate key={item.id} url={item.url} Arther={item.author} width={item.width} height={item.height} />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
-        <div className='coverrft'>
-            <div>
-            <Header/>
-            </div>
-            <div className='content'></div>
-        </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Cover
+export default Cover;
